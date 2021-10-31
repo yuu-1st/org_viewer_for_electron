420192f178ba712d9d2ba7b99ed32eefaee5880d
yuuyu <65267311+yuu-1st@users.noreply.github.com>
2021-10-31 19:21:57 +0900

    Refactoring preload

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,33 +1,11 @@
 // https://zenn.dev/sprout2000/articles/7d2644bb4e198e
 // https://dev.classmethod.jp/articles/typings-of-window-object/
-import {
-  fileChangeFromOrgToHTML,
-  fileOpenToEmacs,
-  FileOperating_CreateNewFile,
-  FileOperating_DeleteFile,
-  getDefaultData,
-  getDirectoryList,
-  ipcRendererOnShowLicenseList,
-  OpenHTML,
-  pathChangeFromRelativeToAbsolute,
-} from '../preload';
-
-export default interface Api {
-  getDirectoryList: typeof getDirectoryList;
-  fileOpenToEmacs: typeof fileOpenToEmacs;
-  getDefaultData: typeof getDefaultData;
-  fileChangeFromOrgToHTML: typeof fileChangeFromOrgToHTML;
-  pathChangeFromRelativeToAbsolute: typeof pathChangeFromRelativeToAbsolute;
-  ipcRendererOnShowLicenseList: typeof ipcRendererOnShowLicenseList;
-  FileOperating_CreateNewFile: typeof FileOperating_CreateNewFile;
-  FileOperating_DeleteFile: typeof FileOperating_DeleteFile;
-  OpenHTML: typeof OpenHTML;
-}
+import { preloadObject } from '../preload';
 
 // global の名前空間にある定義を上書き
 declare global {
   // eslint-disable-next-line no-unused-vars
   interface Window {
-    api: Api;
+    api: typeof preloadObject;
   }
 }
--- src/preload.ts
+++ src/preload.ts
@@ -1,86 +1,122 @@
 import { contextBridge, ipcRenderer } from 'electron';
 import { ApiResultData, DefaultData, DirectoryData } from './@types/connectionDataType';
 
-export async function getDirectoryList(
-  dirName: string,
-  dirLevel: number,
-  isAll: number
-): Promise<DirectoryData[] | null> {
-  const DirList: DirectoryData[] | null = await ipcRenderer.invoke(
-    'getDirectoryList',
-    dirName,
-    dirLevel,
-    isAll
-  );
-  return DirList;
-}
+export const preloadObject = {
+  /**
+   * orgファイルをpandocを経由してHTML形式に変更します。
+   * @param dirPath 変更するorgファイルの絶対パス
+   * @returns dataはstring型
+   */
+  fileChangeFromOrgToHTML: async (dirName: string): Promise<ApiResultData> => {
+    const Message: ApiResultData = await ipcRenderer.invoke('fileChangeFromOrgToHTML', dirName);
+    return Message;
+  },
 
-export async function fileOpenToEmacs(dirName: string): Promise<ApiResultData> {
-  const Message: ApiResultData = await ipcRenderer.invoke('fileOpenToEmacs', dirName);
-  return Message;
-}
+  /**
+   * orgファイルをGUI版Emacsで開きます。
+   * @param dirPath 表示するorgファイルの絶対パス
+   * @returns
+   */
+  fileOpenToEmacs: async (dirName: string): Promise<ApiResultData> => {
+    const Message: ApiResultData = await ipcRenderer.invoke('fileOpenToEmacs', dirName);
+    return Message;
+  },
 
-export async function getDefaultData(): Promise<DefaultData> {
-  const Message: DefaultData = await ipcRenderer.invoke('getDefaultData');
-  return Message;
-}
+  /**
+   * orgファイルを新規作成します。
+   * @param filename ファイル名
+   * @param directory ディレクトリ名
+   * @returns
+   */
+  FileOperating_CreateNewFile: async (
+    filename: string,
+    directory: string
+  ): Promise<ApiResultData> => {
+    const Message: ApiResultData = await ipcRenderer.invoke(
+      'FileOperating_CreateNewFile',
+      filename,
+      directory
+    );
+    return Message;
+  },
 
-export async function fileChangeFromOrgToHTML(dirName: string): Promise<ApiResultData> {
-  const Message: ApiResultData = await ipcRenderer.invoke('fileChangeFromOrgToHTML', dirName);
-  return Message;
-}
+  /**
+   * orgファイルを削除します。
+   * @param fullPath 削除するファイルのフルパス
+   * @returns
+   */
+  FileOperating_DeleteFile: async (fullPath: string): Promise<ApiResultData> => {
+    const Message: ApiResultData = await ipcRenderer.invoke('FileOperating_DeleteFile', fullPath);
+    return Message;
+  },
 
-export async function pathChangeFromRelativeToAbsolute(
-  changePath: string,
-  originalPath: string
-): Promise<string> {
-  const Message: string = await ipcRenderer.invoke(
-    'pathChangeFromRelativeToAbsolute',
-    changePath,
-    originalPath
-  );
-  return Message;
-}
+  /**
+   * 起動時にレンダープロセス側に送信するデフォルトデータを作成します。
+   * @returns
+   */
+  getDefaultData: async (): Promise<DefaultData> => {
+    const Message: DefaultData = await ipcRenderer.invoke('getDefaultData');
+    return Message;
+  },
 
-export async function FileOperating_CreateNewFile(
-  filename: string,
-  directory: string
-): Promise<ApiResultData> {
-  const Message: ApiResultData = await ipcRenderer.invoke(
-    'FileOperating_CreateNewFile',
-    filename,
-    directory
-  );
-  return Message;
-}
+  /**
+   * 指定されたディレクトリリストを出力します。
+   * @param dirPath 表示するディレクトリ。絶対パスもしくは相対パス
+   * @param level 表示する階層数。1以上が必要です
+   * @param isAll 全て表示するか。1の場合はorgファイルとディレクトリのみ、2の場合は隠しファイル以外、3は全てのファイルが表示されます。
+   * @returns
+   */
+  getDirectoryList: async (
+    dirName: string,
+    dirLevel: number,
+    isAll: number
+  ): Promise<DirectoryData[] | null> => {
+    const DirList: DirectoryData[] | null = await ipcRenderer.invoke(
+      'getDirectoryList',
+      dirName,
+      dirLevel,
+      isAll
+    );
+    return DirList;
+  },
 
-export async function FileOperating_DeleteFile(fullPath: string): Promise<ApiResultData> {
-  const Message: ApiResultData = await ipcRenderer.invoke('FileOperating_DeleteFile', fullPath);
-  return Message;
-}
+  /**
+   * 与えられたパスが相対パスだった場合、絶対パスに書き換えます。
+   * 外部のurlを指定する場合、http、https、fileから始まる場合のみ除外されます。それ以外は予期せぬ動作をする可能性があります。
+   * @param changePath 変更対象となるパス
+   * @param originalPath changePathが記述されているファイルの絶対パス
+   * @returns
+   */
 
-export async function OpenHTML(link: string): Promise<void> {
-  await ipcRenderer.invoke('OpenHTML', link);
-}
+  pathChangeFromRelativeToAbsolute: async (
+    changePath: string,
+    originalPath: string
+  ): Promise<string> => {
+    const Message: string = await ipcRenderer.invoke(
+      'pathChangeFromRelativeToAbsolute',
+      changePath,
+      originalPath
+    );
+    return Message;
+  },
 
-/**
- * メインプロセスから呼び出した時に呼び出す関数を登録する関数
- * @param callback
- */
-export function ipcRendererOnShowLicenseList(callback: (text: string) => void): void {
-  ipcRenderer.on('showLicenseList', (event, text) => {
-    callback(text);
-  });
-}
+  /**
+   * linkをブラウザで開きます。
+   * @param link
+   */
+  OpenHTML: async (link: string): Promise<void> => {
+    await ipcRenderer.invoke('OpenHTML', link);
+  },
 
-contextBridge.exposeInMainWorld('api', {
-  getDirectoryList: getDirectoryList,
-  fileOpenToEmacs: fileOpenToEmacs,
-  getDefaultData: getDefaultData,
-  fileChangeFromOrgToHTML: fileChangeFromOrgToHTML,
-  pathChangeFromRelativeToAbsolute: pathChangeFromRelativeToAbsolute,
-  ipcRendererOnShowLicenseList: ipcRendererOnShowLicenseList,
-  FileOperating_CreateNewFile,
-  FileOperating_DeleteFile,
-  OpenHTML,
-});
+  /**
+   * メインプロセスから呼び出した時に呼び出す関数を登録する関数
+   * @param callback
+   */
+  ipcRendererOnShowLicenseList: (callback: (text: string) => void): void => {
+    ipcRenderer.on('showLicenseList', (event, text) => {
+      callback(text);
+    });
+  },
+};
+
+contextBridge.exposeInMainWorld('api', preloadObject);
