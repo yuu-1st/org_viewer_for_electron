f6f7a543e218209114d281a203926f52a33bb53b
yuuyu <65267311+yuu-1st@users.noreply.github.com>
2021-10-31 19:22:03 +0900

    add comment

--- src/main/OpenHTML.ts
+++ src/main/OpenHTML.ts
@@ -1,5 +1,10 @@
 import { shell }  from 'electron';
 
+/**
+ * linkをブラウザで開きます。
+ * @param event
+ * @param link
+ */
 export const OpenHTML = async (
   event: Electron.IpcMainInvokeEvent,
   link: string,
